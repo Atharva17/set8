91e5aba58220913641c70ec1da8c66a924967140
Harshini A <harshinidurai98@gmail.com>
2018-01-23 22:02:35 +0530

    Create in2intervals.c

---
+++ checktheno/in2intervals.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+           int i,N,L,R;
+           scanf("%d%d%d",&N,&L,&R);
+           for(i=L;i<=R;i++)
+           if(i==N)
+           printf("yes");
+           	return 0;
+}
